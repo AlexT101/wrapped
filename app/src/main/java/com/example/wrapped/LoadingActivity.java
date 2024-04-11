1945ba3345025bc96837b9a0d2179da792b5cfa2
mtitov-05 <156847894+mtitov-05@users.noreply.github.com>
2024-04-11 10:28:41 -0400

    Update LoadingActivity.java

--- app/src/main/java/com/example/wrapped/LoadingActivity.java
+++ app/src/main/java/com/example/wrapped/LoadingActivity.java
@@ -62,8 +62,8 @@ public class LoadingActivity extends Activity {
 
 
     private void spotifyAPICall() {
-        Spotify.instance.fetchTopTracks();
         Spotify.instance.fetchTopArtists();
+        Spotify.instance.fetchTopTracks();
         checkAnimation();
     }
 }
